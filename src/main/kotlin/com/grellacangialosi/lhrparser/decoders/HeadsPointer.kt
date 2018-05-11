747d873408512c83c40f43295588a5f30577cf8f
LHRParser Authors <>
2018-05-11 23:45:03 +0200

    Rewrite algorithm

--- src/main/kotlin/com/grellacangialosi/lhrparser/decoders/HeadsPointer.kt
+++ src/main/kotlin/com/grellacangialosi/lhrparser/decoders/HeadsPointer.kt
@@ -1,63 +1,103 @@
+/* Copyright 2018-present LHRParser Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * -----------------------------------------------------------------------------*/
+
 package com.grellacangialosi.lhrparser.decoders
 
+import com.grellacangialosi.lhrparser.LatentSyntacticStructure
+import com.grellacangialosi.lhrparser.utils.ArcScores
+import com.grellacangialosi.lhrparser.utils.ArcScores.Companion.rootId
 import com.kotlinnlp.simplednn.core.functionalities.losses.SoftmaxCrossEntropyCalculator
-import com.kotlinnlp.simplednn.deeplearning.pointernetwork.PointerNetwork
-import com.kotlinnlp.simplednn.deeplearning.pointernetwork.PointerNetworkModel
-import com.kotlinnlp.simplednn.deeplearning.pointernetwork.PointerNetworkParameters
+import com.kotlinnlp.simplednn.attention.pointernetwork.PointerNetwork
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
 import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArrayFactory
 
 /**
  * The HeadsPointer decoder.
  *
- * @param model the model
+ * @param network the pointer network
  */
-class HeadsPointer(model: PointerNetworkModel) {
+class HeadsPointer(
+  private val network: PointerNetwork
+) {
 
   /**
-   * The pointer network.
-   * TODO: fix usage
+   * Calculate the arc-scores.
+   *
+   * @param lss the latent syntactic structure to decode
+   *
+   * @return the computed scores
    */
-  val network = PointerNetwork(model)
+  fun decode(lss: LatentSyntacticStructure): ArcScores {
 
-  /**
-   * @param tokensEncodings tokens encodings
-   * @param goldHeads the heads of the gold dependency tree
-   */
-  fun learn(tokensEncodings: Array<DenseNDArray>, goldHeads: Array<Int?>) {
+    val scores = mutableMapOf<Int, MutableMap<Int, Double>>()
 
-    this.network.setInputSequence(tokensEncodings.toList())
+    this.network.setInputSequence(lss.contextVectors.toList())
 
-    val errorsList = tokensEncodings.mapIndexed { index, tokenEncoding ->
+    lss.latentHeads.forEachIndexed { tokenIndex, latentHead ->
 
-      val prediction = this.network.forward(tokenEncoding)
+      val prediction: DenseNDArray = this.network.forward(latentHead)
 
-      val expectedValues = DenseNDArrayFactory.oneHotEncoder(
-        length = tokensEncodings.size,
-        oneAt = goldHeads[index] ?: index) // self-root
+      scores[tokenIndex] = mutableMapOf()
 
-      SoftmaxCrossEntropyCalculator().calculateErrors(output = prediction, outputGold = expectedValues)
+      (0 until prediction.length).forEach { headIndex ->
+
+        if (tokenIndex == headIndex) {
+          scores.getValue(tokenIndex)[rootId] = prediction[headIndex]
+        } else {
+          scores.getValue(tokenIndex)[headIndex] = prediction[headIndex]
+        }
+      }
     }
 
-    this.network.backward(errorsList)
+    return ArcScores(scores)
+  }
+
+  /**
+   * @param lss the latent syntactic structure
+   * @param goldHeads the heads of the gold dependency tree
+   */
+  fun learn(lss: LatentSyntacticStructure, goldHeads: Array<Int?>) {
+
+    this.network.setInputSequence(lss.contextVectors.toList())
+    this.network.backward(this.calculateErrors(lss.latentHeads, goldHeads))
   }
 
   /**
    * @return the params errors
    */
-  fun getParamsErrors(copy: Boolean = true): PointerNetworkParameters = this.network.getParamsErrors(copy = copy)
+  fun getParamsErrors(copy: Boolean = true) = this.network.getParamsErrors(copy = copy)
 
   /**
-   * @return the input errors
+   * @return the errors of the latent heads
    */
-  fun getInputErrors(): Array<DenseNDArray> {
+  fun getLatentHeadsErrors(): Array<DenseNDArray> = this.network.getInputErrors().inputVectorsErrors.toTypedArray()
 
-    val inputErrors = this.network.getInputErrors()
+  /**
+   * @return the errors of the context vectors
+   */
+  fun getContextVectorsErrors(): Array<DenseNDArray> = this.network.getInputErrors().inputSequenceErrors.toTypedArray()
 
-    require(inputErrors.decodingSequenceErrors.size == inputErrors.inputSequenceErrors.size )
+  /**
+   * @param latentHeads the latent heads
+   * @param goldHeads the heads of the gold dependency tree
+   */
+  private fun calculateErrors(latentHeads: List<DenseNDArray>, goldHeads: Array<Int?>): List<DenseNDArray> {
 
-    return inputErrors.decodingSequenceErrors.zip(inputErrors.inputSequenceErrors).map {
-      it.first.sum(it.second)
-    }.toTypedArray()
+    return latentHeads.mapIndexed { index, latentHead ->
+
+      val prediction: DenseNDArray = this.network.forward(latentHead)
+
+      val expectedValues = DenseNDArrayFactory.oneHotEncoder(
+        length = this.network.inputSequenceSize,
+        oneAt = goldHeads[index] ?: index) // self-root
+
+      SoftmaxCrossEntropyCalculator().calculateErrors(
+        output = prediction,
+        outputGold = expectedValues)
+    }
   }
 }
